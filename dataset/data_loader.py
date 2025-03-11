b370da741a8d4c4d5dcf2592470df5c86d68a1ad
xxxgqcoder <xxxgqcoder@gmail.com>
2025-03-11 11:07:32 +0800

    pretrain dataset loader

---
+++ dataset/data_loader.py
@@ -0,0 +1,44 @@
+import os
+import json
+
+import torch
+from torch.utils.data import Dataset
+
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
+
+class PretrainDataset(Dataset):
+
+    def __init__(self, data_path, tokenizer, max_length=512):
+        super().__init__()
+        self.tokenizer = tokenizer
+        self.max_length = max_length
+        self.samples = self.load_data(data_path)
+
+    def load_data(self, path):
+        samples = []
+        with open(path, 'r', encoding='utf-8') as f:
+            for _, line in enumerate(f, 1):
+                data = json.loads(line.strip())
+                samples.append(data['text'])
+        print(f'finish loading data from {path}, total {len(samples)} samples')
+        return samples
+
+    def __len__(self):
+        return len(self.samples)
+
+    def __getitem__(self, index):
+        sample = self.samples[index]
+
+        text = f"{self.tokenizer.bos_token}{sample}{self.tokenizer.eos_token}"
+        encoding = self.tokenizer(text,
+                                  max_length=self.max_length,
+                                  padding='max_length',
+                                  truncation=True,
+                                  return_tensors='pt')
+        input_ids = encoding.input_ids.squeeze()
+        loss_mask = (input_ids != self.tokenizer.pad_token_id)
+
+        X = torch.tensor(input_ids[:-1], dtype=torch.long)
+        Y = torch.tensor(input_ids[1:], dtype=torch.long)
+        return X, Y, loss_mask
