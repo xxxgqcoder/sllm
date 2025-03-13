803ba435cede81948512d7ea3e88eaf24634f325
xxxgqcoder <xxxgqcoder@gmail.com>
2025-03-13 11:08:13 +0800

    format script

---
+++ format.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e 
+
+cd $(dirname "$0")
+
+# format python
+find . -type f -name '*py' \( -not -path "*/__pycache__/*" -and -not -path "*/.git/*" -and -not -path "*/ipynb_checkpoints/*" \) -prune \
+    -exec yapf -i --style .style.yapf {} +
