4b728d10e9865d44a28adf756a7a08cc143fb62a
xxxgqcoder <xxxgqcoder@gmail.com>
2025-03-18 15:21:54 +0800

    change jupter port

--- start_jupyter.sh
+++ start_jupyter.sh
@@ -2,9 +2,10 @@
 set -e
 cd $(dirname "$0")
 
-ps ax | grep -E "jupyter-lab" | grep -v "grep" | awk '{print $1}' | xargs -I {} kill -9 {}
-
+port_num=2003
 jupyter_log_file='.logs/jupyter.logs'
+
+ps ax | grep -E "jupyter-lab --port=${port_num}" | grep -v "grep" | awk '{print $1}' | xargs -I {} kill -9 {}
 
 rm -r ${jupyter_log_file} || True
 
@@ -12,7 +13,6 @@ mkdir -p $(dirname ${jupyter_log_file})
 
 touch ${jupyter_log_file}
 
-port_num=3456
 nohup jupyter-lab --port=${port_num}> ${jupyter_log_file} 2>&1 &
 
 echo 'jupyter-lab is running'
